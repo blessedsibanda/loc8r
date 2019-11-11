18ad612e0835fbc1bc19d6ced0a682f32ab222d3
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-11-11 11:50:42 +0200

    move data out of views to controllers

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -35,7 +35,34 @@ const homelist = (req, res) => {
 
 const locationInfo = (req, res) => {
 	res.render('location-info', {
-		src: 'https://maps.google.com/maps?q=Bulawayo,%20Zimbabwe&t=&z=14&ie=UTF8&iwloc=&output=embed'
+		src: 'https://maps.google.com/maps?q=Bulawayo,%20Zimbabwe&t=&z=14&ie=UTF8&iwloc=&output=embed',
+		title: 'Loc8r - find a place to work with wifi',
+		location: {
+			name: 'Starcups Name',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 3,
+			facilities: [ 'Hot drinks', 'Food', 'Premium wifi' ],
+			distance: '100m',
+			reviews: [
+				{
+					rating: 4,
+					author: 'Blessed Sibanda',
+					timestamp: '11 October 2019',
+					content: 'What a great place'
+				},
+				{
+					rating: 2,
+					author: 'Simon Holmes',
+					timestamp: '02 October 2019',
+					content: "It was okay. Coffee wasn't great."
+				}
+			],
+			openingTimes: [
+				{ days: 'Monday - Friday', time: '7:00am - 7:00pm' },
+				{ days: 'Saturday', time: '8:00am - 5:00pm' },
+				{ days: 'Sunday', time: 'closed' }
+			]
+		}
 	});
 };
 
